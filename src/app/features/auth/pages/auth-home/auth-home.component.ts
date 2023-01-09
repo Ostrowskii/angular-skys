08a85ceefaff6703852dce11dce956a97297930f
Ostrowskii <46391664+Ostrowskii@users.noreply.github.com>
2023-01-08 23:07:05 -0400

    feature: mudancas em auth home

--- src/app/features/auth/pages/auth-home/auth-home.component.ts
+++ src/app/features/auth/pages/auth-home/auth-home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth-home',
@@ -7,7 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AuthHomeComponent {
 
+  constructor(private router: Router){
 
-
-
+  }
+  acessarPagina(){
+    this.router.navigate(["/register"])
+  }
 }
