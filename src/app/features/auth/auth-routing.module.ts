c8cfc2ffc5d6365663797a694068e6354eec5557
Ostrowskii <46391664+Ostrowskii@users.noreply.github.com>
2023-01-08 23:11:05 -0400

    feature: auth routing

--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthSwiperComponent } from './components/auth-swiper/auth-swiper.component';
 import { AuthHomeComponent } from './pages/auth-home/auth-home.component';
+import { AuthQuizComponent } from './pages/auth-quiz/auth-quiz.component';
+import { AuthRegisterComponent } from './pages/auth-register/auth-register.component';
 
 const routes: Routes = [
   {path:"home", component: AuthHomeComponent},
-  {path:"sw", component: AuthSwiperComponent}
+  {path:"register", component: AuthRegisterComponent},
+  {path:"sw", component: AuthSwiperComponent},
+  {path:"quiz", component: AuthQuizComponent}
 ];
 
 @NgModule({
--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -6,11 +6,17 @@ import { AuthHomeComponent } from './pages/auth-home/auth-home.component';
 import { SharedModule } from "../../shared/shared.module";
 import { AuthSwiperComponent } from './components/auth-swiper/auth-swiper.component';
 import { SwiperModule } from "swiper/angular";
+import { AuthRegisterComponent } from './pages/auth-register/auth-register.component';
+import { AuthQuizComponent } from './pages/auth-quiz/auth-quiz.component';
+import { AuthBarraPaginaComponent } from './components/auth-barra-pagina/auth-barra-pagina.component';
 
 @NgModule({
     declarations: [
         AuthHomeComponent,
-        AuthSwiperComponent
+        AuthSwiperComponent,
+        AuthRegisterComponent,
+        AuthQuizComponent,
+        AuthBarraPaginaComponent
     ],
     imports: [
         CommonModule,
