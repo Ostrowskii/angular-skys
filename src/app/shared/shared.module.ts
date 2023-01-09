4c75f9b50d03aa368a2bb76756de926bdab63617
Ostrowskii <46391664+Ostrowskii@users.noreply.github.com>
2023-01-08 23:10:44 -0400

    feature: componentes compartilhados

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BotaoTransparenteBordaBrancaComponent } from './botao-transparente-borda-branca/botao-transparente-borda-branca.component';
 import { InicioRoutingModule } from '../features/inicio/inicio-routing.module';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { ListSelectComponent } from './list-select/list-select.component';
 // import { SwiperModule } from "swiper/angular";
 
 
 @NgModule({
   declarations: [
-    BotaoTransparenteBordaBrancaComponent
+    BotaoTransparenteBordaBrancaComponent,
+    ListSelectComponent,
   ],
   imports: [
     CommonModule,
     InicioRoutingModule,
+    ReactiveFormsModule,
     // SwiperModule
   ],
   exports: [
     BotaoTransparenteBordaBrancaComponent,
+    ReactiveFormsModule,
+    ListSelectComponent,
     // SwiperModule
   ]
 })
